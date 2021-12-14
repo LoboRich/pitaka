a126b8acaaf3f74cbfafa4f1f6a7977931a973ac
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-14 19:29:26 +0800

    Validates account names precense to true

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
 	has_one :user
   	accepts_nested_attributes_for :user
   	enum roles: { admin: "admin", broker: "broker", trader: "trader"}
+  	validates :firstname, :lastname, presence: true
 end
