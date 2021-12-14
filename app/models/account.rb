68d9489c3ebfc2b6a1b03a150183c07308ce5f16
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-14 19:45:48 +0800

    Scaffolds wallet

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,10 @@ class Account < ApplicationRecord
 	has_one :user
   	accepts_nested_attributes_for :user
   	enum roles: { admin: "admin", broker: "broker", trader: "trader"}
+
+	# after_create :create_wallet
+
+	# def create_wallet
+	# 	Wallet.create()
+	# end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :wallets
   resources :markets
   devise_for :users
   resources :accounts
