56e58551a168b3eee5a9d585c999d95fb37f74f5
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-14 20:11:57 +0800

    Migrate Logs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_12_14_115029) do
 
   # These are extensions that must be enabled in order to support this database
@@ -36,15 +35,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_115029) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "market_portfolios", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "portfolio_id"
-    t.uuid "market_id"
-    t.float "stocks"
-    t.float "revenue"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "markets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "logo"
     t.string "name"
@@ -52,12 +42,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_115029) do
     t.text "description"
     t.float "buying_price"
     t.float "selling_price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "portfolios", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
