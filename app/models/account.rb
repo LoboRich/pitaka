8a868b1198ba059b03d7ae9f4e3e3e5ec7aaf1a5
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2021-12-14 20:01:20 +0800

    scaffolds Log; add relationship to log and account

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,12 @@ class Account < ApplicationRecord
 	has_one :user
   	accepts_nested_attributes_for :user
   	enum roles: { admin: "admin", broker: "broker", trader: "trader"}
+
+	has_many :logs
+
+	after_create :create_log 
+
+	def create_log 
+		Log.create(account:self, description: "Account created.")
+	end
 end
