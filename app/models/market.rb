e0c58a04b94dcf66fffa5cf85d0513820d342bf1
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2021-12-14 17:05:39 +0800

    add initial validations to Market

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,2 +1,6 @@
 class Market < ApplicationRecord
+  mount_uploader :logo, AvatarUploader
+  validates :name, uniqueness: { scope: :name, message: "Market name already exists." }
+  validates :symbol, presence: true 
+  validates :description, presence: true
 end
