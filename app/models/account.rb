4901b9890ca3d91319287117eebefd0f723e83b1
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-14 19:58:40 +0800

    Adds relationship to account and wallet, and automatically create wallet after account creation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,13 @@
 class Account < ApplicationRecord
 	mount_uploader :img, AvatarUploader
 	has_one :user
+	has_one :wallet
   	accepts_nested_attributes_for :user
   	enum roles: { admin: "admin", broker: "broker", trader: "trader"}
 
-	# after_create :create_wallet
+	after_create :create_wallet
 
-	# def create_wallet
-	# 	Wallet.create()
-	# end
+	def create_wallet
+		Wallet.create(account: self, balance: "100k")
+	end
 end
