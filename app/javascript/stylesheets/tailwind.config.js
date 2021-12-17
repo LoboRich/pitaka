f8faedf121334753897414b82e05cbabab46d6b0
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2021-12-17 19:28:52 +0800

    add all color hex to be used

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      "dark-brown": "#683b2b",
+      "light-brown": "#ded2bd",
+      white: "#faf6f2",
+      black: "#343434",
+      pink: "#d49e8d",
+      gold: "#b08401",
+    },
     extend: {},
   },
   variants: {
