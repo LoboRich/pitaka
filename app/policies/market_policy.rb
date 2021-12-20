b74585e0ba91a9fb67fcd76116dcaf58ee4b53b0
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-20 15:00:50 +0800

    Add market policy file

---
+++ app/policies/market_policy.rb
@@ -0,0 +1,35 @@
+class MarketPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def buy?
+    [
+      'broker',
+      'trader'
+    ].include?(user.role)
+  end
+
+  def actions?
+    [
+      'admin'
+    ].include?(user.role)
+  end
+  
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
+  
