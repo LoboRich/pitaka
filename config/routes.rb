896aa5565a4be0d9e49728149eb59189b2b20a86
Rich Lobo <lobo.r0018@gmail.com>
2022-01-08 11:22:41 +0800

    Fix string to float for computation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :users
   resources :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  namespace :api do 
+    get '/today_quote', to: 'quotes#index'
+  end
   root 'accounts#index'
 end
