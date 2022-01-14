a5fb47d815664c3d549bf5d1cb8b8b22dd195f64
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2022-01-14 19:58:35 +0800

    fix ui bugs

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnight: "#1c345c",
+      },
+    },
   },
   variants: {
     extend: {},
